9a1394e8a945639619e10b06b2f2d4a3851b7d15
hiramatsu-yasu <hiramatsu-yasu@dmm.com>
2018-09-03 15:34:19 +0900

    iframeの修正、アニメーションをお手製のものに変更

--- mock.js
+++ mock.js
@@ -36,19 +36,12 @@ $(function(){
             $('.fn-point05').addClass('is-tween');
         }
 
-        if (windowBottom > 2600) {
-            show($('.fn-point06'));
-            $('.fn-point06').addClass('animated fadeIn');
-        }
-
         if (windowBottom > 3000) {
-            show($('.fn-point07'));
-            $('.fn-point07').addClass('animated fadeIn');
+            $('.fn-point06').addClass('is-tween');
         }
 
-        if (windowBottom > 3200) {
-            show($('.fn-point08'));
-            $('.fn-point08').addClass('animated fadeIn');
+        if (windowBottom > 3300) {
+            $('.fn-point07').addClass('is-tween');
         }
 
     });
@@ -59,35 +52,8 @@ $(function(){
      * 初期処理
      */
     function init() {
-
-        // 非表示
-        // hide($('.fn-point01'));
-        // hide($('.fn-point02'));
-        // hide($('.fn-point03'));
-        // hide($('.fn-point04'));
-        // hide($('.fn-point05'));
-        hide($('.fn-point06'));
-        hide($('.fn-point07'));
-        hide($('.fn-point08'));
-
         // メインビジュアルのアニメーション
         $('.fn-pulseIn').addClass('is-tween');
-    }
-
-    /**
-     * 要素の表示
-     * @param {jQuery Object} $obj
-     */
-    function show($obj) {
-        $obj.css("opacity", "1");
-    }
-
-    /**
-     * 要素の非表示
-     * @param {jQuery Object} $obj
-     */
-    function hide($obj) {
-        $obj.css("opacity", "0");
     }
 
 });
